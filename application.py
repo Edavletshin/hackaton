3914fee4d5fc2782676553091771d084f1634011
Петр Свалов <petrsvalov@MacBook-Pro-Petr.local>
2019-06-15 12:54:19 +0300

    bot

--- application.py
+++ application.py
@@ -1,9 +1,42 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from telegram import Bot
+from telegram import Update
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
 
-@app.route('/hello/<user>')
-def hello_name(user):
-   return render_template('hello.html', name = user)
+from botkavovka.config import TG_TOKEN
+
+def do_start(bot: Bot, update: Update):
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Привет! Я хочу увидеть, как ты приседаешь! Я подскажу, если ты медленно убиваешь свои колени или что-то еще",
+    )
+
+
+def do_echo(bot: Bot, update: Update):
+    text = update.message.text
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Ты что делаешь??? Ты что??? Ты видос должен кидать, а не баллады писать  ",
+    )
+
+def main():
+    bot = Bot(
+        token=TG_TOKEN,
+    )
+    updater = Updater(
+        bot=bot,
+    )
+
+    start_handler = CommandHandler("start", do_start)
+    message_handler = MessageHandler(Filters.text, do_echo)
+
+    updater.dispatcher.add_handler(start_handler)
+    updater.dispatcher.add_handler(message_handler)
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    main()
