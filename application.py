c6f5e6733a6ed1f5b68626bea07efb7b147807ac
Петр Свалов <petrsvalov@MacBook-Pro-Petr.local>
2019-06-15 13:18:16 +0300

    dva

--- application.py
+++ application.py
@@ -7,36 +7,43 @@ from telegram.ext import Filters
 
 from botkavovka.config import TG_TOKEN
 
-def do_start(bot: Bot, update: Update):
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Привет! Я хочу увидеть, как ты приседаешь! Я подскажу, если ты медленно убиваешь свои колени или что-то еще",
-    )
-
-
-def do_echo(bot: Bot, update: Update):
-    text = update.message.text
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Ты что делаешь??? Ты что??? Ты видос должен кидать, а не баллады писать  ",
-    )
-
-def main():
-    bot = Bot(
-        token=TG_TOKEN,
-    )
-    updater = Updater(
-        bot=bot,
-    )
-
-    start_handler = CommandHandler("start", do_start)
-    message_handler = MessageHandler(Filters.text, do_echo)
-
-    updater.dispatcher.add_handler(start_handler)
-    updater.dispatcher.add_handler(message_handler)
-
-    updater.start_polling()
-    updater.idle()
+@app.route('/hello/<user>')
+def hello_name(user):
+   return render_template('hello.html', name = user)
 
 if __name__ == '__main__':
-    main()
+   app.run(debug = True)
+   app.run()
+# def do_start(bot: Bot, update: Update):
+#     bot.send_message(
+#         chat_id=update.message.chat_id,
+#         text="Привет! Я хочу увидеть, как ты приседаешь! Я подскажу, если ты медленно убиваешь свои колени или что-то еще",
+#     )
+
+
+# def do_echo(bot: Bot, update: Update):
+#     text = update.message.text
+#     bot.send_message(
+#         chat_id=update.message.chat_id,
+#         text="Ты что делаешь??? Ты что??? Ты видос должен кидать, а не баллады писать  ",
+#     )
+
+# def main():
+#     bot = Bot(
+#         token=TG_TOKEN,
+#     )
+#     updater = Updater(
+#         bot=bot,
+#     )
+
+#     start_handler = CommandHandler("start", do_start)
+#     message_handler = MessageHandler(Filters.text, do_echo)
+
+#     updater.dispatcher.add_handler(start_handler)
+#     updater.dispatcher.add_handler(message_handler)
+
+#     updater.start_polling()
+#     updater.idle()
+
+# if __name__ == '__main__':
+#     main()
