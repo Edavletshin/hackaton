77bcc3eb2dcf3b2cab836a60e5570084b165754b
Петр Свалов <petrsvalov@MacBook-Pro-Petr.local>
2019-06-15 14:00:39 +0300

    asds

--- application.py
+++ application.py
@@ -13,36 +13,36 @@ TG_TOKEN = "849661734:AAEppr89hTnLZ0qRV3KacJAE8hRKV88Gx5E"
 def hello_name(user):
    return render_template('hello.html', name = user)
 
-def do_start(bot: Bot, update: Update):
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Привет! Я хочу увидеть, как ты приседаешь! Я подскажу, если ты медленно убиваешь свои колени или что-то еще",
-    )
+# def do_start(bot: Bot, update: Update):
+#     bot.send_message(
+#         chat_id=update.message.chat_id,
+#         text="Привет! Я хочу увидеть, как ты приседаешь! Я подскажу, если ты медленно убиваешь свои колени или что-то еще",
+#     )
 
 
-def do_echo(bot: Bot, update: Update):
-    text = update.message.text
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Ты что делаешь??? Ты что??? Ты видос должен кидать, а не баллады писать  ",
-    )
+# def do_echo(bot: Bot, update: Update):
+#     text = update.message.text
+#     bot.send_message(
+#         chat_id=update.message.chat_id,
+#         text="Ты что делаешь??? Ты что??? Ты видос должен кидать, а не баллады писать  ",
+#     )
 
-def main():
-    bot = Bot(
-        token=TG_TOKEN,
-    )
-    updater = Updater(
-        bot=bot,
-    )
+# def main():
+#     bot = Bot(
+#         token=TG_TOKEN,
+#     )
+#     updater = Updater(
+#         bot=bot,
+#     )
 
-    start_handler = CommandHandler("start", do_start)
-    message_handler = MessageHandler(Filters.text, do_echo)
+#     start_handler = CommandHandler("start", do_start)
+#     message_handler = MessageHandler(Filters.text, do_echo)
 
-    updater.dispatcher.add_handler(start_handler)
-    updater.dispatcher.add_handler(message_handler)
+#     updater.dispatcher.add_handler(start_handler)
+#     updater.dispatcher.add_handler(message_handler)
 
-    updater.start_polling()
-    updater.idle()
+#     updater.start_polling()
+#     updater.idle()
 
 if __name__ == '__main__':
 #    main()
