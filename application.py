0b6a1e684a378ad34827191c177455529589a98c
Петр Свалов <petrsvalov@MacBook-Pro-Petr.local>
2019-06-15 12:27:01 +0300

    qwe

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!'
+@app.route('/hello/<user>')
+def hello_name(user):
+   return render_template('hello.html', name = user)
+
+if __name__ == '__main__':
+   app.run(debug = True)
